35f3e60d463aa11f6bd1c0c33858117ca7775a35
Javier Contreras <javierc@fnal.gov>
2025-07-30 16:54:01 -0500

    Initial SPI controller implementation

--- fw/srcs/vhdl/modules/mod-ads9813-spi/spi_ads9813.vhd
+++ fw/srcs/vhdl/modules/mod-ads9813-spi/spi_ads9813.vhd
@@ -14,13 +14,13 @@
 -- Project    :
 -------------------------------------------------------------------------------
 -- File       : spi_ads9813.vhd
--- Author     :   <javierc@correlator6.fnal.gov>
+-- Author     : Javier Contreras 52425N
 -- Division   : CSAID/RTPS/DIS
 -- Created    : 2025-07-29
 -- Last update: 2025-07-30
--- Standard   : VHDL'93/02
+-- Standard   : VHDL'08
 -------------------------------------------------------------------------------
--- Description: Generates SPI transactions for ADC configuration
+-- Description: Generates SPI transactions for ADC configuration. TX Only
 -------------------------------------------------------------------------------
 -- Copyright (c) 2025 Fermi Forward Discovery Group, LLC
 -------------------------------------------------------------------------------
@@ -34,14 +34,18 @@ use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
 use work.ads9813_pkg.all;
+use work.qps_pkg.all;
 
 entity SpiController_ADS9813 is
 
   port (
-    clk             : in  std_logic;
-    FunctionAddress : in  t_enum_ADS9813_SPI_FUNCTIONS;
-    trigger         : in  std_logic;
-    if_Spi          : inout t_SPI_MGT);
+    clk              : in    std_logic;
+    FunctionAddress  : in    t_enum_ADS9813_SPI_FUNCTIONS;
+    triggerTx        : in    std_logic;
+    triggerRx        : in    std_logic;
+    readData         : out   std_logic_vector(23 downto 0);
+    if_AdcUserConfig : in    t_ADC_USER_CONFIG;
+    if_Spi           : inout t_SPI_MGT);
 
 end entity SpiController_ADS9813;
 
@@ -50,30 +54,75 @@ architecture rtl of SpiController_ADS9813 is
   -- <State Machine>
   type t_fsm_CommunicationState is (st_IDLE, st_TX, st_WAIT, st_RX);
   signal fsm_CommunicationState : t_fsm_CommunicationState;
+  signal sig_TxDone             : std_logic := '0';
   -- </.>
 
   -- <Transmission FIFO>
-  signal sig_TxBuffer : t_arr_SPI_TX_BUFFER(0 to 9);
-  signal sig_TxPointer : unsigned(5 downto 0);
+  signal sig_TxBuffer  : t_arr_SPI_TX_BUFFER(0 to 9);
+  signal sig_TxPointer : t_POINTER;
   -- </.>
 
-  procedure proc_InitAdc (
-    signal if_Spi : inout t_SPI_MGT;
-    signal clk    : in    std_logic);
+  procedure proc_BufferTx(
+    signal arg_Buffer  : in    t_arr_SPI_TX_BUFFER;
+    signal arg_Pointer : inout t_POINTER;
+    signal arg_Done    : out   std_logic;
+    signal arg_Spi     : inout t_SPI_MGT
+    ) is
+  begin
+    arg_Spi.tx_trn <= '0';              -- Default
+
+    if arg_Pointer > 0 and arg_Spi.busy = '0' then
+      arg_Spi.addr    <= arg_Buffer(arg_Pointer).address;
+      arg_Spi.wr_data <= arg_Buffer(arg_Pointer).data;
+      arg_Spi.tx_trn  <= '1';
+      arg_Pointer     <= arg_Pointer - 1;
+    end if;
+
+    if arg_Pointer = 0 and arg_Spi.busy = '0' then
+      arg_Done <= '1';
+    end if;
+
+  end procedure proc_BufferTx;
+
+  procedure proc_BufferPush(
+    signal arg_Buffer  : inout t_arr_SPI_TX_BUFFER;
+    signal arg_Pointer : inout t_POINTER;
+    constant arg_Data  : in    t_rec_SPI_TX_WORD
+    ) is
+  begin
+    arg_Pointer             <= arg_Pointer + 1;
+    arg_Buffer(arg_Pointer) <= arg_Data;
+  end procedure proc_BufferPush;
+
+  procedure proc_BufferReset(
+    signal arg_Buffer  : inout t_arr_SPI_TX_BUFFER;
+    signal arg_Pointer : out   t_POINTER
+    ) is
+  begin
+    for idx in 0 to arg_Buffer'length - 1 loop
+      arg_Buffer(idx) <= (address => x"00", data => x"0000");
+    end loop;
+    arg_Pointer <= 0;
+  end procedure proc_BufferReset;
 
 begin  -- architecture rtl
 
-  p_fsm_Communication: process(clk) is
+  p_fsm_Communication : process(clk) is
   begin
     if rising_edge(clk) then
       case fsm_CommunicationState is
         when st_IDLE =>
-          null;
+          if triggerTx = '1' then
+            fsm_CommunicationState <= st_TX;
+          end if;
         when st_TX =>
-          fsm_CommunicationState <= st_WAIT;
-        when st_WAIT =>
-          null;
+          proc_BufferTx(sig_TxBuffer, sig_TxPointer, sig_TxDone, if_Spi);
+          if sig_TxDone = '1' then
+            fsm_CommunicationState <= st_IDLE;
+            proc_BufferReset(sig_TxBuffer, sig_TxPointer);
+          end if;
         when st_RX =>
+          -- Not implemented
           null;
         when others =>
           null;
@@ -81,33 +130,71 @@ begin  -- architecture rtl
     end if;
   end process p_fsm_Communication;
 
-  p_CommunicationWait : process (clk) is
-    begin
-      if rising_edge(clk) then
-        -- if not if_Spi.busy
-      end if;
-    end process p_CommunicationWait;
-
   p_FunctionDecode : process(clk) is
+
+    -- Name shortening aliases
+    alias ptr  is sig_TxPointer;
+    alias buf  is sig_TxBuffer;
+    alias spi  is c_data_SpiCommands;
+
+    variable v_Cmd : t_rec_SPI_TX_WORD;
+
   begin
 
     if rising_edge(clk) then
-      if trigger then
+      if triggerTx = '1' and fsm_CommunicationState = st_IDLE then
         case functionAddress is
           when en_FUNCTION_NONE =>
             null;
           when en_FUNCTION_INIT =>
-            sig_TxBuffer(0) <= c_data_SpiCommands(cmd_INIT_1);
-            sig_TxBuffer(1) <= c_data_SpiCommands(cmd_SEL_B2);
-            sig_TxBuffer(2) <= c_data_SpiCommands(cmd_B2_INIT_2);
-            sig_TxBuffer(3) <= c_data_SpiCommands(cmd_B2_INIT_3);
-            null;
-          when en_FUNCTION_SET_TEST =>
-            null;
-          when en_FUNCTION_UNSET_TEST =>
-            null;
+            proc_BufferPush(buf, ptr, spi(cmd_INIT_1));
+            proc_BufferPush(buf, ptr, spi(cmd_SEL_B2));
+            proc_BufferPush(buf, ptr, spi(cmd_B2_INIT_2));
+            proc_BufferPush(buf, ptr, spi(cmd_B2_INIT_3));
+          when en_FUNCTION_ENABLE_TEST =>
+            proc_BufferPush(buf, ptr, spi(cmd_SEL_B1));
+            proc_BufferPush(buf, ptr, spi(cmd_B1_TEST_PATTERN_ENABLE_CH1_4));
+            proc_BufferPush(buf, ptr, spi(cmd_B1_TEST_PATTERN_ENABLE_CH5_8));
+            proc_BufferPush(buf, ptr, spi(cmd_B1_TEST_PATTERN_SET_1OF4));
+            proc_BufferPush(buf, ptr, spi(cmd_B1_TEST_PATTERN_SET_2OF4));
+            proc_BufferPush(buf, ptr, spi(cmd_B1_TEST_PATTERN_SET_3OF4));
+            proc_BufferPush(buf, ptr, spi(cmd_B1_TEST_PATTERN_SET_4OF4));
+          when en_FUNCTION_DISABLE_TEST =>
+            proc_BufferPush(buf, ptr, spi(cmd_B1_TEST_PATTERN_DISABLE_CH1_4));
+            proc_BufferPush(buf, ptr, spi(cmd_B1_TEST_PATTERN_DISABLE_CH5_8));
           when en_FUNCTION_SET_VOLT_SCALE =>
+            v_Cmd := spi(cmd_B1_VOLTSCALE_CH1_4);
+
+            -- Set proper config for desired voltage scale
+            with if_AdcUserConfig.VoltageScale select
+                v_Cmd.data := x"0000" when en_VOLTSCALE_5V0,
+                              x"1111" when en_VOLTSCALE_3V5,
+                              x"2222" when en_VOLTSCALE_2V5,
+                              x"3333" when en_VOLTSCALE_7V0,
+                              x"4444" when en_VOLTSCALE_10V0,
+                              x"5555" when en_VOLTSCALE_12V0;
+            proc_BufferPush(buf, ptr, v_Cmd);
+
+            -- Repeat for channels 5-8
+            v_Cmd.address := spi(cmd_B1_VOLTSCALE_CH5_8).address;
+            proc_BufferPush(buf, ptr, v_Cmd);
             null;
+          when en_FUNCTION_SET_TEST_PATTERN =>
+            v_Cmd := spi(cmd_B1_TEST_PATTERN_SET_1OF4);
+            v_Cmd.data := if_AdcUserConfig.TestPatternCh1To4(15 downto 0);
+            proc_BufferPush(buf, ptr, v_Cmd);
+
+            v_Cmd := spi(cmd_B1_TEST_PATTERN_SET_2OF4);
+            v_Cmd.data := x"00" & if_AdcUserConfig.TestPatternCh1To4(23 downto 16);
+            proc_BufferPush(buf, ptr, v_Cmd);
+
+            v_Cmd := spi(cmd_B1_TEST_PATTERN_SET_3OF4);
+            v_Cmd.data := if_AdcUserConfig.TestPatternCh5To8(23 downto 16);
+            proc_BufferPush(buf, ptr, v_Cmd);
+
+            v_Cmd := spi(cmd_B1_TEST_PATTERN_SET_4OF4);
+            v_Cmd.data := x"00" & if_AdcUserConfig.TestPatternCh5To8(23 downto 16);
+            proc_BufferPush(buf, ptr, v_Cmd);
           when others =>
             null;
         end case;
