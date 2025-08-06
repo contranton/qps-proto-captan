0665c14ef48817d62e41f4e3241087a5497b24ec
Javier Contreras <javierc@fnal.gov>
2025-08-06 15:08:27 -0500

    Synthesis runs

--- fw/scripts/scr_BuildNonProject.tcl
+++ fw/scripts/scr_BuildNonProject.tcl
@@ -15,11 +15,12 @@ proc find_files file_ext {
 
 # Vivado commands start below
 
-set_part xc7a200tffg1156-2L
+set_part xc7a200tffg1156-2
 
-read_vhdl -vhdl2008 [find_files vhd]
-read_verilog [find_files v]
-read_verilog -sv [find_files sv]
+read_vhdl    -vhdl2008 [find_files vhd]
+read_verilog           [find_files v]
+read_verilog -sv       [find_files sv]
+read_ip                [find_files xcix]
 
 set_property top main [get_filesets sources_1]
 
